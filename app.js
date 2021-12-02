de20807ed1fd455cf843daa4569dc276c056c8ae
HarryP <harrypham__@hotmail.com>
2021-12-02 15:15:06 +1100

    no overwriting

--- app.js
+++ app.js
@@ -25,13 +25,13 @@ function test(event){
         console.log(clickedOn)
         clickedOn.style.backgroundColor="red"
         playerOne.score.push(clickedOn.id)
-
+        clickedOn.disabled = true
     } else {
         console.log("odd")
         console.log(clickedOn)
         clickedOn.style.backgroundColor="blue"
         playerTwo.score.push(clickedOn.id)
-
+        clickedOn.disabled = true
 
     }
     counterTurn = counterTurn + 1
@@ -39,49 +39,70 @@ function test(event){
     if (playerOne.score.includes("cell1") && playerOne.score.includes("cell2") && playerOne.score.includes("cell3")) {
         victoryText.textContent = "Player 1 wins !"
         console.log("Player 1 wins with cells: 1, 2, 3")
+        disableAll()
     } else if (playerOne.score.includes("cell1") && playerOne.score.includes("cell4") && playerOne.score.includes("cell7")) {
         victoryText.textContent = "Player 1 wins !"
         console.log("Player 1 wins with cells: 1, 4, 7")
+        disableAll()
     } else if (playerOne.score.includes("cell1") && playerOne.score.includes("cell5") && playerOne.score.includes("cell9")) {
         victoryText.textContent = "Player 1 wins !"
         console.log("Player 1 wins with cells: 1, 5, 9")
+        disableAll()
     } else if (playerOne.score.includes("cell2") && playerOne.score.includes("cell5") && playerOne.score.includes("cell8")) {
         victoryText.textContent = "Player 1 wins !"
         console.log("Player 1 wins with cells: 2, 5, 8")
+        disableAll()
     } else if (playerOne.score.includes("cell3") && playerOne.score.includes("cell5") && playerOne.score.includes("cell7")) {
         victoryText.textContent = "Player 1 wins !"
         console.log("Player 1 wins with cells: 3, 5, 7")
+        disableAll()
     } else if (playerOne.score.includes("cell4") && playerOne.score.includes("cell5") && playerOne.score.includes("cell6")) {
         victoryText.textContent = "Player 1 wins !"
         console.log("Player 1 wins with cells: 4, 5, 6")
+        disableAll()
     } else if (playerOne.score.includes("cell7") && playerOne.score.includes("cell8") && playerOne.score.includes("cell9")) {
         victoryText.textContent = "Player 1 wins !"
         console.log("Player 1 wins with cells: 7, 8, 9")
+        disableAll()
     } else if (playerTwo.score.includes("cell1") && playerTwo.score.includes("cell2") && playerTwo.score.includes("cell3")) {
         victoryText.textContent = "Player 2 wins !"
         console.log("Player 2 wins with cells: 1, 2, 3")
+        disableAll()
     } else if (playerTwo.score.includes("cell1") && playerTwo.score.includes("cell4") && playerTwo.score.includes("cell7")) {
         victoryText.textContent = "Player 2 wins !"
         console.log("Player 2 wins with cells: 1, 4, 7")
+        disableAll()
     } else if (playerTwo.score.includes("cell1") && playerTwo.score.includes("cell5") && playerTwo.score.includes("cell9")) {
         victoryText.textContent = "Player 2 wins !"
         console.log("Player 2 wins with cells: 1, 5, 9")
+        disableAll()
     } else if (playerTwo.score.includes("cell2") && playerTwo.score.includes("cell5") && playerTwo.score.includes("cell8")) {
         victoryText.textContent = "Player 2 wins !"
         console.log("Player 2 wins with cells: 2, 5, 8")
+        disableAll()
     } else if (playerTwo.score.includes("cell3") && playerTwo.score.includes("cell5") && playerTwo.score.includes("cell7")) {
         victoryText.textContent = "Player 2 wins !"
         console.log("Player 2 wins with cells: 3, 5, 7")
+        disableAll()
     } else if (playerTwo.score.includes("cell4") && playerTwo.score.includes("cell5") && playerTwo.score.includes("cell6")) {
         victoryText.textContent = "Player 2 wins !"
         console.log("Player 2 wins with cells: 4, 5, 6")
+        disableAll()
     } else if (playerTwo.score.includes("cell7") && playerTwo.score.includes("cell8") && playerTwo.score.includes("cell9")) {
         victoryText.textContent = "Player 2 wins !"
         console.log("Player 2 wins with cells: 7, 8, 9")
+        disableAll()
     }
 }
 
+function disableAll(){
+    let i = 0
+    while(i < allSquares.length) {
+        allSquares[i].disabled = true;
+        i++;
+    }
 
+}
 
 //callbacks
 allSquares.forEach(function(square){
